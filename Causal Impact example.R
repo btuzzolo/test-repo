da167bbaf71127c7e08a576b46f8675dca9afe52
Bruno <btuzzolo@users.noreply.github.com>
2023-08-15 16:05:59 -0300

    Update Causal Impact example.R

--- Causal Impact example.R
+++ Causal Impact example.R
@@ -1,38 +1,38 @@
+###########################################################################
+# Exemplo de aplicação do método Causal Impact, que gera predições e dados
+# 'contra-factuais', criando uma base de comparação caso a ação de Marketing/Promoção
+# ou outro tipo de intervenção não tivesse acontecido
+##########################################################################
+## setup do diretório de trabalho
 getwd()
-setwd("C:/Users/Bruno/Documents/Bruno/Coursera")
+setwd("C:/Users/")
 
+## Instalando as bibliotecas necessárias
 install.packages("CausalImpact")
 library(CausalImpact)
 library(ggplot2)
 
-
-set.seed(1)
+## Gerando série temporal hipotética para servir de exemplo
+set.seed(100)
 x1 = 100 + arima.sim(model = list(ar = 0.999), n = 150)
-
-help(arima.sim)
-
 y = 1.5 * x1 + rnorm(150)
-
 y[101:135] = y[101:135] + 12
+date.points = seq.Date(as.Date("2023-01-01") , by = 1 , length.out = 150)
+data = zoo(cbind(y, x1), date.points)
 
-data = cbind(y, x1)
+## Plotando a série temporal gerada
+plot(data[ ,1], type = "l" , ylim =c(110,max(data[ ,1])) 
+     , col = "black" , lwd = 2.0 , xlab = ("Tempo") , ylab = ("Observações") )
+abline(v = as.Date("2023-04-11"), col = "red" , type = "l" , lty = 2)
 
-head(data)
+## Definindo o período antes (pré) e após (post) o início da intervenção (campanha)
+preperiod = as.Date(c("2023-01-01" , "2023-04-10"))
+postperiod = as.Date(c("2023-04-11" , "2023-05-30"))
 
-matplot(data, type = "l")
-
-
-
-impact.plot = plot(impact) + theme_bw(base_size = 20)
-plot(impact.plot)
-
-
-preperiod <- c(1, 70)
-postperiod <- c(71, 100)
-
+## Rodando a análise dos dados via função 'CausalImpact'
 impact = CausalImpact(data, preperiod, postperiod)
-
 plot(impact)
 
+## Resumo dos resultados da análise
 summary(impact)
 summary(impact, "report")
